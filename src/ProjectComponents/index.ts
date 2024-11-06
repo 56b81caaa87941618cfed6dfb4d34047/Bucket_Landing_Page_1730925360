a5620f0d47c5a6ccb34c0d19145f3952321b0944
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-11-06 14:36:37 -0600

    Add component: Footer1730925397

--- src/ProjectComponents/index.ts
+++ src/ProjectComponents/index.ts
@@ -1,3 +1,4 @@
 export * as Hero1730925363 from './Hero1730925363'
 export * as Header1730925363 from './Header1730925363'
+export * as Footer1730925397 from './Footer1730925397'
 export * as Footer1730925363 from './Footer1730925363'
